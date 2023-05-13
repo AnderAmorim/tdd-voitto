16d4108beec1069a8fe3e6e9a99570fb78781c9b
Anderson Amorim Dos Santos Oliveira <anderson.amorim.s@N0137388.bbrouter>
2023-05-13 15:01:17 -0300

    m2-lesson9

---
+++ m2-lesson9/src/components/Highlight.test.js
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react";
+import Highlight from "./Highlight";
+
+test("renders a value", () => {
+  const value = "3000";
+  render(<Highlight value={value} />);
+  expect(screen.getByText(value)).toBeInTheDocument();
+});
+
+test("renders a value", () => {
+  const value = "5000";
+  render(<Highlight value={value} />);
+  expect(screen.getByText(value)).toBeInTheDocument();
+});
